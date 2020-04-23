fb95a30fb588633ba4d280f713cac64a55159cb7
zhwchen <zhwchen2401@163.com>
2020-04-23 09:43:21 +0800

    完成了按键循环点灯

--- bare_devel/led/led.c
+++ bare_devel/led/led.c
@@ -1,5 +1,5 @@
-#include "../MCIMX6Y2/MCIMX6Y2.h"
-#include "../MCIMX6Y2/drivers/fsl_iomuxc.h"
+#include "MCIMX6Y2.h"
+#include "fsl_iomuxc.h"
 #include "pad_config.h"
 
 
@@ -16,9 +16,22 @@
 #define RGB_BLUE_LED_GPIO_PIN           (19U)
 #define RGB_BLUE_LED_IOMUXC             IOMUXC_CSI_VSYNC_GPIO4_IO19
 
+/*按键2 GPIO端口、引脚号及IOMUXC复用宏定义*/
+#define button2_GPIO               GPIO5
+#define button2_GPIO_PIN           (1U)
+#define button2_IOMUXC             IOMUXC_SNVS_SNVS_TAMPER1_GPIO5_IO01
 
 
-/* 所有引脚均使用同样的PAD配置 */
+/* LED的PAD配置 */
+/* 配置说明 : 
+    转换速率: 转换速率慢
+    驱动强度: R0/6 
+    带宽配置 : medium(100MHz)
+    开漏配置: 关闭 
+    拉/保持器配置: 关闭
+    拉/保持器选择: 保持器（上面已关闭，配置无效）
+    上拉/下拉选择: 100K欧姆下拉（上面已关闭，配置无效）
+    滞回器配置: 关闭 */
 #define LED_PAD_CONFIG_DATA            (SRE_0_SLOW_SLEW_RATE| \
                                         DSE_6_R0_6| \
                                         SPEED_2_MEDIUM_100MHz| \
@@ -27,15 +40,27 @@
                                         PUE_0_KEEPER_SELECTED| \
                                         PUS_0_100K_OHM_PULL_DOWN| \
                                         HYS_0_HYSTERESIS_DISABLED)   
-    /* 配置说明 : */
-    /* 转换速率: 转换速率慢
-      驱动强度: R0/6 
-      带宽配置 : medium(100MHz)
-      开漏配置: 关闭 
-      拉/保持器配置: 关闭
-      拉/保持器选择: 保持器（上面已关闭，配置无效）
-      上拉/下拉选择: 100K欧姆下拉（上面已关闭，配置无效）
-      滞回器配置: 关闭 */  
+
+
+/* 按键PAD配置 */
+/* 配置说明 : 
+    转换速率: 转换速率慢
+    驱动强度: R0/6 
+    带宽配置 : medium(100MHz)
+    开漏配置: 关闭 
+    拉/保持器配置: 关闭
+    拉/保持器选择: 保持器（上面已关闭，配置无效）
+    上拉/下拉选择: 100K欧姆下拉（上面已关闭，配置无效）
+    滞回器配置: 开启 */
+#define button_PAD_CONFIG_DATA            (SRE_0_SLOW_SLEW_RATE| \
+                                        DSE_6_R0_6| \
+                                        SPEED_2_MEDIUM_100MHz| \
+                                        ODE_0_OPEN_DRAIN_DISABLED| \
+                                        PKE_0_PULL_KEEPER_DISABLED| \
+                                        PUE_0_KEEPER_SELECTED| \
+                                        PUS_0_100K_OHM_PULL_DOWN| \
+                                        HYS_1_HYSTERESIS_ENABLED)   
+
 
 
 /*简单延时函数*/
@@ -75,24 +100,71 @@ int main()
     GPIO4->GDIR |= (1<<19);  //设置GPIO4_19为输出模式
     GPIO4->DR |= (1<<19);    //设置GPIO4_19输出电平为高电平
 
-    while(1)
-    {
-         GPIO1->DR &= ~(1<<4); //红灯亮
-         delay(0xFFFFF);
-         GPIO1->DR |= (1<<4); //红灯灭
+    /*按键初始化*/
+    CCM_CCGR1_CG15(0x3);  //开启GPIO5的时钟
 
-         GPIO4->DR &= ~(1<<20); //绿灯亮
-         delay(0xFFFFF);
-         GPIO4->DR |= (1<<20); //绿灯灭
+    /*设置 绿灯 引脚的复用功能以及PAD属性*/
+    IOMUXC_SetPinMux(button2_IOMUXC,0);     
+    IOMUXC_SetPinConfig(button2_IOMUXC, button_PAD_CONFIG_DATA); 
 
-         GPIO4->DR &= ~(1<<19); //蓝灯亮
-         delay(0xFFFFF);
-         GPIO4->DR |= (1<<19); //蓝灯灭
+    GPIO5->GDIR &= ~(1<<1);  //设置GPIO5_01为输入模式
+
+    // while(1)
+    // {
+    //      GPIO1->DR &= ~(1<<4); //红灯亮
+    //      delay(0xFFFFF);
+    //      GPIO1->DR |= (1<<4); //红灯灭
+
+    //      GPIO4->DR &= ~(1<<20); //绿灯亮
+    //      delay(0xFFFFF);
+    //      GPIO4->DR |= (1<<20); //绿灯灭
+
+    //      GPIO4->DR &= ~(1<<19); //蓝灯亮
+    //      delay(0xFFFFF);
+    //      GPIO4->DR |= (1<<19); //蓝灯灭
+    // }
+    /* 有按键按下，执行红色led灯翻转 */
+    // if ((GPIO1->DR)&(1<<4)) {
+        // GPIO1->DR &= ~(1<<4);    //绿灯亮
+    //} else {
+        //GPIO1->DR |= (1<<4);     //绿灯灭
+    //}
+    //break;
+    uint8_t color_cnt = 0;
+
+    while (1) {
+        if ((GPIO5->DR)&(1<<1)) {
+            color_cnt++;
+            if (color_cnt >= 3) {
+                color_cnt = 0;
+            }
+
+            switch (color_cnt) {
+            case 0:
+                GPIO4->DR |= (1<<20);   // 红灯灭
+                GPIO4->DR |= (1<<19);   // 蓝灯灭
+
+                GPIO1->DR &= ~(1<<4);   //绿灯亮
+                break;
+            case 1:
+                GPIO1->DR |= (1<<4);    // 绿灯灭
+                GPIO4->DR |= (1<<19);   // 蓝灯灭
+
+                GPIO4->DR &= ~(1<<20);  // 红灯亮
+                break;
+            case 2:
+                GPIO4->DR |= (1<<20);   // 红灯灭
+                GPIO1->DR |= (1<<4);    // 绿灯灭
+
+                GPIO4->DR &= ~(1<<19);  // 蓝灯亮
+                break;
+            default:
+                break;
+            }
+
+            while((GPIO5->DR)&(1<<1));//等待按键松开
+        }
     }
 
     return 0;    
 }
-
-
-
-
